b1a6a3210fb2fd55538c14b0afbc6b10e9011e35
JakeGuinto <116121157+JakeGuinto@users.noreply.github.com>
2024-04-11 13:49:35 -0700

    ScriptedPatient
    finished scripting for the patient

--- AIGoalDrivenBehavior/Assets/Scripts/GAgent.cs
+++ AIGoalDrivenBehavior/Assets/Scripts/GAgent.cs
@@ -27,16 +27,82 @@ public class GAgent : MonoBehaviour
     SubGoal currentGoal;
 
     // Start is called before the first frame update
-    void Start()
+    public void Start()
     {
         GAction[] acts = this.GetComponents<GAction>();
         foreach (GAction a in acts)
             actions.Add(a);
     }
 
-  
+    bool invoked = false;
+    void completeAction()
+    {
+        currentAction.running = false;
+        currentAction.PostPerform();
+        invoked = false;
+    }
+
     void LateUpdate()
     {
+        if(currentAction != null && currentAction.running) 
+        {
+            float distanceToTarget = Vector3.Distance(currentAction.target.transform.position, this.transform.position);
+            if(currentAction.agent.hasPath && currentAction.agent.remainingDistance < 2f)
+            {
+                if(!invoked)
+                {
+                    Invoke("CompleteAction", currentAction.duration);
+                    invoked = true;
+                }
+            }
+            return;
+        }
         
+        if (planner == null || actionQueue == null)
+        {
+            planner = new GPlanner();
+
+            var sortedGoals = from entry in goals orderby entry.Value descending select entry;
+
+            foreach(KeyValuePair<SubGoal, int> sg in sortedGoals)
+            {
+                actionQueue = planner.plan(actions, sg.Key.sgoals, null);
+                    if(actionQueue != null)
+                {
+                    currentGoal = sg.Key;
+                    break;
+                }
+            }
+        }
+
+        if(actionQueue != null && actionQueue.Count == 0)
+        {
+            if (currentGoal.remove) 
+            {  
+                goals.Remove(currentGoal);
+            }
+            planner = null;
+        }
+
+        if(actionQueue != null && actionQueue.Count > 0)
+        {
+            currentAction = actionQueue.Dequeue();
+            if(currentAction.PrePerform())
+            {
+                if (currentAction.target == null && currentAction.targetTag != "")
+                    currentAction.target = GameObject.FindWithTag(currentAction.targetTag);
+
+                if(currentAction.targetTag != null)
+                {
+                    currentAction.running = true;
+                    currentAction.agent.SetDestination(currentAction.target.transform.position);
+                }    
+            }
+            else
+            {
+                actionQueue = null;
+            }
+        }
+
     }
 }
